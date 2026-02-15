d1952d1729699fd0b143a431a81a78b91b417bc0
naufalmaula <naufal.maula@gmail.com>
2026-02-15 09:58:46 +0700

    add 06 D. 06 E.

---
+++ 06 D. Jual-Beli Bebek III/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int beli[10] = {13, 100, 0, 4, 31, 0, 178, 23, 1, 13};
+    int jual[10] = {0, 2, 24, 0, 10, 4, 0, 121, 0, 15};
+
+    int sum = 0;
+
+    for (int i = 0; i < 10; i++) {
+        sum = sum + beli[i];
+        sum = sum - jual[i];
+        cout << sum << endl;
+    }
+}
